965c1d17e7f3b87e6a02098e96ce139eed168ee5
Andrei Huk <belakolala1@gmail.com>
2022-04-06 23:27:36 +0300

    Добавьте файлы проекта.

---
+++ Task_14_Console/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using Types;
+
+namespace Task_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var people1 = new People("Andrew", "hud", 1544);
+            var people2 = new People("андрей", "хзхз", 18);
+            Console.WriteLine(ValidateString(people1));
+            Console.WriteLine(ValidateString(people2));
+
+            Console.ReadLine();
+        }
+        static string ValidateString(People people)
+        {
+            Type t = typeof(People);
+            PropertyInfo[] properties = t.GetProperties(BindingFlags.Instance | BindingFlags.Public);
+            string resault = string.Empty;
+            foreach (PropertyInfo property in properties)
+            {
+                foreach (ValidateAtribute attr in property.GetCustomAttributes(false))
+                {
+                    if (Regex.IsMatch(people.Name, attr.Pattern) && Regex.IsMatch(people.LastName, attr.Pattern))
+                        resault = "Data is correct!";
+                    else
+                        resault = "Data is not correct!";
+                }
+            }
+            return resault;
+        }
+    }
+}
---
+++ Types/Class1.cs
@@ -0,0 +1,26 @@
+﻿namespace Types
+{
+    [AttributeUsage(AttributeTargets.All)]
+    public class ValidateAtribute : Attribute
+    {
+        public string Pattern { get; set; }
+        public ValidateAtribute(string pattern)
+        {
+            Pattern = pattern;
+        }
+    }
+    public class People
+    {
+        [ValidateAtribute($"^[a-zA-Z]+$")]
+        public string Name { get; set; }
+        [ValidateAtribute($"^[a-zA-Z]+$")]
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public People(string name, string lastName, int age)
+        {
+            Name = name;
+            LastName = lastName;
+            Age = age;
+        }
+    }
+}
